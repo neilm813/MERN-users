b7a8f4dc421ba5996f30f0c9154709b4b8cc2feb
Neil M <neilm813@gmail.com>
2020-09-17 12:21:13 -0700

    pckg ref

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,34 +5,35 @@ const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: [true, "Username name is required"]
+      required: [true, "Username name is required"],
     },
+    // https://www.npmjs.com/package/mongoose-type-email
     email: {
       type: String,
-      required: [true, "Email is required"]
+      required: [true, "Email is required"],
     },
     password: {
       type: String,
       required: [true, "Password is required"],
-      minlength: [8, "Password must be 8 characters or longer"]
-    }
+      minlength: [8, "Password must be 8 characters or longer"],
+    },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 UserSchema.virtual("confirmPassword")
   .get(() => this._confirmPassword)
-  .set(value => (this._confirmPassword = value));
+  .set((value) => (this._confirmPassword = value));
 
-UserSchema.pre("validate", function(next) {
+UserSchema.pre("validate", function (next) {
   if (this.password !== this.confirmPassword) {
     this.invalidate("confirmPassword", "Password must match confirm password");
   }
   next();
 });
 
-UserSchema.pre("save", function(next) {
-  bcrypt.hash(this.password, 10).then(hash => {
+UserSchema.pre("save", function (next) {
+  bcrypt.hash(this.password, 10).then((hash) => {
     this.password = hash;
     next();
   });
